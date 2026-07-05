535b9742a16f9975b7695ea7b6feca1c632fdc57
coderTanishkaJaiswal02 <tanishkaj290@gmail.com>
2026-07-05 17:04:38 +0530

    Initial commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,3 +6,4 @@ export default defineConfig([
   { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
   { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
 ]);
+git 
