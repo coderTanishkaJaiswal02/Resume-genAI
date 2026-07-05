9488ed50c1c847d023a4dd8475617c031a9147c0
coderTanishkaJaiswal02 <tanishkaj290@gmail.com>
2026-07-05 17:09:15 +0530

    Fresh start

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,4 +6,3 @@ export default defineConfig([
   { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
   { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
 ]);
-git 
